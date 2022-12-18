83f87d3d42c2512cefb148bc3eb64ed5b67abf24
nat <85887826+NatUSB@users.noreply.github.com>
2022-12-18 09:30:20 -0300

    Create hex.cpp

---
+++ hex.cpp
@@ -0,0 +1,28 @@
+static std::string hex2str(std::string hex)
+{
+    int len = (int)hex.length();
+    std::string s;
+    for (int i = 0; i < len; i += 2)
+    {
+        std::string byte = hex.substr(i, 2);
+        char chr = (char)(int)strtol(byte.c_str(), NULL, 16);
+        s.push_back(chr);
+    }
+    return s;
+}
+
+static std::string str2hex(const std::string& input)
+{
+    static const char* const lut = "0123456789ABCDEF";
+    size_t len = input.length();
+
+    std::string output;
+    output.reserve(2 * len);
+    for (size_t i = 0; i < len; ++i)
+    {
+        const unsigned char c = input[i];
+        output.push_back(lut[c >> 4]);
+        output.push_back(lut[c & 15]);
+    }
+    return output;
+}
